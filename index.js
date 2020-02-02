a20560907dd6e13b2ce59d34fd84458d6e6bbf79
duru <enfn2001@gmail.com>
2020-02-02 14:45:18 +0900

    ls -al 명령어 실행

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 
 var folder  = require('./lib/cli.js');
 var cdates = require('./lib/custom-date.js');
 var File = require('./lib/file-count.js');
+var exec = require('child_process').exec;
 
 
-if(folder(cdates()+'_'+File.cntFolder(cdates())))
-console.log("Created Month Folder : " + cdates()+'_'+File.cntFolder(cdates()));
+var newDirName =  cdates()+'_'+File.cntFolder(cdates());
+if(folder(newDirName))
+    console.log("Created Month Folder : " + newDirName);
+
+var commands = [
+    `ls -al`
+]
+
+exec(commands.join('&&'), function callback(error, stdout, stderr) {
+    if (error) {
+        console.log('error : ' + error);
+    }
+
+    console.log("execute ls -al");
+});
